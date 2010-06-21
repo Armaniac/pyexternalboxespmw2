79ba2beb6d684720f15a3eee57f3d1dddaba63ca
drewbirmingham <drewbirmingham@43f26500-2d9e-4698-c95b-a25fc0ebe23a>
2010-06-21 05:51:18 +0000

    I updated the config file along with Status.py in order to allow F1 to toggle visibility of hack display status. If you do not want this in this please disregard and revert back a revision.

--- src/Status.py
+++ src/Status.py
@@ -12,14 +12,15 @@ class Status(object):
         self.env = env
 
     def render(self):
-        frame = self.env.frame
-        read_game = self.env.read_game
-        draw_line(frame.line, 0, read_game.resolution_y - 10, 800, 0, 20, 0xAF000000)
-        
-        for i in range(1, 13):
-            if keys["F"+str(i)]:
-                color = STATUS_COLOR_ACTIVE
-            else:
-                color = STATUS_COLOR_INACTIVE
-            label = "F" + str(i) + ":" + getattr(Config, "F"+str(i)+"_LABEL")
-            draw_string_left(frame.status_font, (i-1)*65 + 5, read_game.resolution_y-15, 65, 15, color, label)
+        if keys["F1"]:
+            frame = self.env.frame
+            read_game = self.env.read_game
+            draw_line(frame.line, 0, read_game.resolution_y - 10, 845, 0, 20, 0xAF000000)
+            
+            for i in range(1, 13):
+                if keys["F"+str(i)]:
+                    color = STATUS_COLOR_ACTIVE
+                else:
+                    color = STATUS_COLOR_INACTIVE
+                label = "F" + str(i) + ":" + getattr(Config, "F"+str(i)+"_LABEL")
+                draw_string_left(frame.status_font, (i-1)*70 + 5, read_game.resolution_y-15, 65, 15, color, label)
