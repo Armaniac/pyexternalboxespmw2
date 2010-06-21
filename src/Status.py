4bfbe979f7218923018d934cccee4a63bfd285f2
drewbirmingham <drewbirmingham@43f26500-2d9e-4698-c95b-a25fc0ebe23a>
2010-06-21 10:45:33 +0000

    DHeir - This commit updates code to make F1 toggle esp bar display visible/non visible. This also updates code to have F2 Toggle the location of the exp bar from bottom left to top left. NOTE: I'd like to at some point after you figure out where you would like the key binds for the glitch hack, to make more keys accessible to the display, for example, make a selectable in game overlay menu for keys. :) obviously this will be display toggleable.

--- src/Status.py
+++ src/Status.py
@@ -6,21 +6,38 @@ from Keys import keys
 #
 # draw the status line
 #
+
 class Status(object):
     
     def __init__(self, env):
         self.env = env
-
+    
     def render(self):
-        if keys["F1"]:
-            frame = self.env.frame
-            read_game = self.env.read_game
-            draw_line(frame.line, 0, read_game.resolution_y - 10, 845, 0, 20, 0xAF000000)
-            
-            for i in range(1, 13):
-                if keys["F"+str(i)]:
-                    color = STATUS_COLOR_ACTIVE
-                else:
-                    color = STATUS_COLOR_INACTIVE
-                label = "F" + str(i) + ":" + getattr(Config, "F"+str(i)+"_LABEL")
-                draw_string_left(frame.status_font, (i-1)*70 + 5, read_game.resolution_y-15, 65, 15, color, label)
+        if not keys["F2"]:
+                if keys["F1"]:
+                    frame = self.env.frame
+                    read_game = self.env.read_game
+                    draw_line(frame.line, 0, read_game.resolution_y - read_game.resolution_y + 10, 845, 0, 20, 0xAF000000)    
+                        
+                    for i in range(1, 13):
+                        if keys["F"+str(i)]:
+                            color = STATUS_COLOR_ACTIVE
+                        else:
+                            color = STATUS_COLOR_INACTIVE
+                        label = "F" + str(i) + ":" + getattr(Config, "F"+str(i)+"_LABEL")
+                        draw_string_left(frame.status_font, (i-1)*70 + 5, read_game.resolution_y - read_game.resolution_y + 3, 65, 15, color, label)
+                        
+        else:    
+                if keys["F1"]:
+                    frame = self.env.frame
+                    read_game = self.env.read_game
+                    draw_line(frame.line, 0, read_game.resolution_y - 10, 845, 0, 20, 0xAF000000)
+                    
+                    for i in range(1, 13):
+                        if keys["F"+str(i)]:
+                            color = STATUS_COLOR_ACTIVE
+                        else:
+                            color = STATUS_COLOR_INACTIVE
+                        label = "F" + str(i) + ":" + getattr(Config, "F"+str(i)+"_LABEL")
+                        draw_string_left(frame.status_font, (i-1)*70 + 5, read_game.resolution_y-15, 65, 15, color, label)                        
+                            
