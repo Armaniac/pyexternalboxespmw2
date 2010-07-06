987354666ac1a1627aaa047e9933c909f19f43cd
sph4ck.mw2 <sph4ck.mw2@43f26500-2d9e-4698-c95b-a25fc0ebe23a>
2010-07-06 09:33:00 +0000

    Slightly enhanced Inspector code

--- src/Inspector.py
+++ src/Inspector.py
@@ -3,6 +3,7 @@ from Config import KEY_INSPECTOR
 from Keys import keys
 from utils import dump_obj
 from ctypes import Structure, c_char
+from structs import ET_EXPLOSIVE, ENTITIESMAX
 # this module allows to inspect entities near the center crosshair
 
 class dumped(Structure):
@@ -18,14 +19,20 @@ class Inspector(object):
         frame = self.env.frame
         #if not read_game.is_in_game or not keys["KEY_INSPECTOR"]: return
         if keys["KEY_INSPECTOR"]: 
-            for e in read_game.mw2_entity.arr:
+            for idx in range(ENTITIESMAX):
+                e = read_game.mw2_entity.arr[idx]
                 spot = read_game.world_to_screen(e.pos)
                 if spot:
                     cur_angle_dist = self.sq(spot.x - read_game.screen_center_x, spot.y - read_game.screen_center_y)
                     if cur_angle_dist < 50 * 50:      # not too far from center
-                        s = "[typ=%i, %i, %i]" % (e.type, e.clientNum, e.WeaponNum) 
+                        s = "[idx=%i, typ=%i, weap=%i]" % (idx, e.type, e.WeaponNum) 
                         draw_string_center(frame.font, spot.x, spot.y, 0xFFFFFFFF, s)
                         print s
+                        #=======================================================
+                        # if e.type == ET_EXPLOSIVE:
+                        #    print "dump explo"
+                        #    print dump_obj(e)
+                        #=======================================================
         
         if keys["KEY_INSPECT_POS"]:                 # print my player's position
             pos = read_game.mw2_mypos
@@ -34,13 +41,17 @@ class Inspector(object):
             print "angles= (%.2f, %.2f, %.2f)" % (ang.x, ang.y, ang.z)
         
         if keys["KEY_INSPECT_DUMP"]:                # dump some memory structures
-            mem = dumped()
+            #mem = dumped()
             #read_game._RPM(0x6727F13, mem)
             #read_game._RPM(0x6727F10, mem)
             #print dump_obj(mem)
-            read_game._RPM(0x64DA350, mem)
-            print dump_obj(mem)
-            del mem
+            #read_game._RPM(0x64DA350, mem)
+            for i in range(4):
+                print "player #%i" % i
+                print dump_obj(read_game.mw2_entity.arr[i])
+                print "client info"
+                print dump_obj(read_game.mw2_clientinfo.arr[i])
+            #del mem
             #===================================================================
             # print "refdef"
             # print dump_obj(read_game.mw2_refdef)
