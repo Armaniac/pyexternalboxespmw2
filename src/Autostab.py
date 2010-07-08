cae919d4debd2459db6c55de56088dc227c2d45d
sph4ck.mw2 <sph4ck.mw2@43f26500-2d9e-4698-c95b-a25fc0ebe23a>
2010-07-08 11:30:54 +0000

    Move stab_glitch to a separate thread. *** to be tested in-game ***

--- src/Autostab.py
+++ src/Autostab.py
@@ -3,33 +3,36 @@ from Keys import keys
 from ctypes import windll
 from structs import ET_PLAYER
 import time
+import thread
 
 KEYEVENTF_KEYUP = 0x0002
+
+def _stab_glitch():
+    windll.User32.keybd_event(0x45, 0x12, 0, 0)
+    windll.User32.keybd_event(0x45, 0x12, KEYEVENTF_KEYUP, 0)
+    time.sleep(.14)
+    windll.User32.keybd_event(0x47, 0x22, 0, 0)
+    time.sleep(.03)
+    windll.User32.keybd_event(0x31, 0x02, 0, 0)
+    windll.User32.keybd_event(0x31, 0x02, KEYEVENTF_KEYUP, 0)
+    time.sleep(.010)
+    windll.User32.keybd_event(0x47, 0x22, KEYEVENTF_KEYUP, 0)
 
 class Autostab(object):
     
     def __init__(self, env):
         self.env = env
         self.last_melee_tick = 0
-        
+    
     def stab_glitch(self):
-        windll.User32.keybd_event(0x45, 0x12, 0, 0)
-        windll.User32.keybd_event(0x45, 0x12, KEYEVENTF_KEYUP, 0)
-        time.sleep(.14)
-        windll.User32.keybd_event(0x47, 0x22, 0, 0)
-        time.sleep(.03)
-        windll.User32.keybd_event(0x31, 0x02, 0, 0)
-        windll.User32.keybd_event(0x31, 0x02, KEYEVENTF_KEYUP, 0)
-        time.sleep(.010)
-        windll.User32.keybd_event(0x47, 0x22, KEYEVENTF_KEYUP, 0)
-        time.sleep(.25)
+        thread.start_new_thread(_stab_glitch, ())
     
     def render(self):
         read_game = self.env.read_game
         if keys["KEY_KNIFE_GLITCH"] and read_game.is_in_game:
-            self.env.ticks - self.last_melee_tick > 5
-            self.last_melee_tick = self.env.ticks
-            self.stab_glitch()
+            if self.env.ticks - self.last_melee_tick > 5:
+                self.last_melee_tick = self.env.ticks
+                self.stab_glitch()
     
         if not read_game.is_in_game or not keys["KEY_AUTOSTAB"]:
             return
