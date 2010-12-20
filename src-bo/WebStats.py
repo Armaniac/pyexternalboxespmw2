ffc7c08b8c81b41e9e9e87cff9858aa57294539d
sph4ck.mw2 <sph4ck.mw2@43f26500-2d9e-4698-c95b-a25fc0ebe23a>
2010-12-20 09:40:55 +0000

    Printing back sensitivity. I have maybe the wrong offset.

--- src-bo/WebStats.py
+++ src-bo/WebStats.py
@@ -36,7 +36,7 @@ class WebStats(object):
             self.params["gm"] = read_game.game_mode
             if not self._prev_is_in_game:
                 print "Starting match mode '%s' for map '%s'" % (self.params["gm"], self.params["map"])
-                #print "Sensitivity = %.1f" % (read_game.sensitivity, )
+                print "Sensitivity = %.1f" % (read_game.sensitivity, )
         else:
             if self._prev_is_in_game:
                 print "Ending match score = %s" % (self.params["score"], )
