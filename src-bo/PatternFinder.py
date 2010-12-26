2dccee0eefb15710b787796b03df5f07e986cc51
sph4ck.mw2 <sph4ck.mw2@43f26500-2d9e-4698-c95b-a25fc0ebe23a>
2010-12-26 14:37:51 +0000

    Finalized pattern scanning.

--- src-bo/PatternFinder.py
+++ src-bo/PatternFinder.py
@@ -1,9 +1,10 @@
 from binascii import unhexlify, hexlify
 import re
 from Config import * #@UnusedWildImport
-from ctypes import create_string_buffer, windll, byref, sizeof, addressof, c_int
+from ctypes import create_string_buffer, windll, byref, sizeof, c_int
 from utils import ExitingException
 from struct import unpack
+import time
 
 PATTERN_START_ADDR = 0x00401000
 PATTERN_LEN = 0x00700000
@@ -29,14 +30,8 @@ SENSITIVITY_DVAR   = 0x00E3CC54
 # "sensitivity" string: => A1951B, string is at +1   0xA1951C
 
 FIND_PATTERNS = { 
-#                  'CG_ClientFrame':             ("83ec3453555657e800000000",
-#                                                 "FFFFFFFFFFFFFFFF00000000"),
-#                  'getWeaponinfo':              ("8b4424048b0c85000000008b4108c3",
-#                                                 "FFFFFFFFFFFFFF00000000FFFFFFFF"),
                   'sensitivity_str':             ("0073656E736974697669747900",
                                                   "FFFFFFFFFFFFFFFFFFFFFFFFFF"),
-                  'sensitivity_dvar':            ("6800000000E800000000D9050000000083C41868000000006A0183EC0CD95C2408A300000000",
-                                                  "FFFFFFFFFFFF00000000FFFF00000000FFFFFFFF00000000FFFFFFFFFFFFFFFFFFFF00000000"),
                   'weapons':                     ("8B5424048B0D0000000033C08D6424003B1485000000007407403BC176F233C0C3",
                                                   "FFFFFFFFFFFF00000000FFFFFFFFFFFFFFFFFF00000000FFFFFFFFFFFFFFFFFFFF"),
                   'cg_t':                        ("8B4424088B0D0000000089818C150700C3",
@@ -46,6 +41,9 @@ FIND_PATTERNS = {
                   'entities':                    ("8B4424083D000400008B4C24047C198B1500000000C1E10903C8",
                                                   "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000FFFFFFFFFF"),
                 }
+
+SENSITIVITY_DVAR_PATTERN = "6800000000E800000000D9050000000083C41868000000006A0183EC0CD95C2408A300000000"
+SENSITIVITY_DVAR_MASK    = "FFFFFFFFFFFF00000000FFFF00000000FFFFFFFF00000000FFFFFFFFFFFFFFFFFFFF00000000"
 
 class PatternFinder(object):
     
@@ -90,69 +88,49 @@ class PatternFinder(object):
         
         print "----------------------------------------"
         
-        print "Finding Sensitivity location, string is at location 0x%x" % self.addr['sensitivity_str']
-        sensitivity_dvar_pattern = FIND_PATTERNS['sensitivity_dvar'][0]
-        sensitivity_dvar_mask = FIND_PATTERNS['sensitivity_dvar'][1]
-        sensitivity_ptr = hexlify(c_int(self.addr['sensitivity_str'] + 1))     # skipping leading 00
-        sensitivity_dvar_pattern = sensitivity_dvar_pattern[:2] + sensitivity_ptr + sensitivity_dvar_pattern[10:]
-        res = self._find_pattern(raw, unhexlify(sensitivity_dvar_pattern), unhexlify(sensitivity_dvar_mask))
-        if len(res) == 1:
-            sensitivity_dvar_code = PATTERN_START_ADDR + res[0]
-            print "Sensitivity DVAR code found 0x%x" % sensitivity_dvar_code
-            self.SENSITIVITY_DVAR = self._get_int_in_raw(raw, sensitivity_dvar_code + 34)
-            print "Sensitivity DVAR found 0x%x, should be 0x%x" % (self.SENSITIVITY_DVAR, SENSITIVITY_DVAR)
+        ok = False
+        while not ok:
+            cg_t_ptr = self._get_int_in_raw(raw, self.addr["cg_t"] + 6)
+            self.CG_T = self._RPM_int(process_handle, cg_t_ptr)
+            print "Found CG_T ptr 0x%x and 0x%x, should be 0x%x" % (cg_t_ptr, self.CG_T, CG_T)
+            self.REFDEF = self.CG_T + 0x42EC0
+            print "Calculated REFDEF 0x%x, should be 0x%x" % (self.REFDEF, REFDEF)
+            
+            cgs_t_ptr = self._get_int_in_raw(raw, self.addr["cgs_t"] + 6)
+            self.CGS_T = self._RPM_int(process_handle, cgs_t_ptr)
+            print "Found CGS_T ptr 0x%x and 0x%x, should be 0x%x" % (cgs_t_ptr, self.CGS_T, CGS_T)
+            
+            entities_ptr = self._get_int_in_raw(raw, self.addr["entities"] + 49)
+            self.ENTITY = self._RPM_int(process_handle, entities_ptr)
+            print "Found ENTITIES ptr 0x%x and 0x%x, should be 0x%x" % (entities_ptr, self.ENTITY, ENTITY)
+            self.CLIENTINFO = self.ENTITY - 0x8CFF4
+            print "Calculated CLIENTINFO 0x%x, should be 0x%x" % (self.CLIENTINFO, CLIENTINFO)
+            
+            if self.CG_T == 0 or self.CGS_T == 0 or self.ENTITY == 0:
+                print "Game not completely loaded, waiting for 2 seconds"
+                print "----------------------------------------"
+                time.sleep(2.0)
+                continue
+            
+            print "Finding Sensitivity location, string is at location 0x%x" % self.addr['sensitivity_str']
+            sensitivity_dvar_pattern = SENSITIVITY_DVAR_PATTERN
+            sensitivity_dvar_mask = SENSITIVITY_DVAR_MASK
+            sensitivity_ptr = hexlify(c_int(self.addr['sensitivity_str'] + 1))     # skipping leading 00
+            sensitivity_dvar_pattern = sensitivity_dvar_pattern[:2] + sensitivity_ptr + sensitivity_dvar_pattern[10:]
+            res = self._find_pattern(raw, unhexlify(sensitivity_dvar_pattern), unhexlify(sensitivity_dvar_mask))
+            if len(res) == 1:
+                sensitivity_dvar_code = PATTERN_START_ADDR + res[0]
+                print "Sensitivity DVAR code found 0x%x" % sensitivity_dvar_code
+                self.SENSITIVITY_DVAR = self._get_int_in_raw(raw, sensitivity_dvar_code + 34)
+                print "Sensitivity DVAR found 0x%x, should be 0x%x" % (self.SENSITIVITY_DVAR, SENSITIVITY_DVAR)
+            
+            self.WEAPON_PTR = self._get_int_in_raw(raw, self.addr["weapons"] + 19)
+            print "Found Weapons 0x%x, should be 0x%x" % (self.WEAPON_PTR, WEAPON_PTR)
         
-        self.WEAPON_PTR = self._get_int_in_raw(raw, self.addr["weapons"] + 19)
-        print "Found Weapons 0x%x, should be 0x%x" % (self.WEAPON_PTR, WEAPON_PTR)
-        
-        cg_t_ptr = self._get_int_in_raw(raw, self.addr["cg_t"] + 6)
-        self.CG_T = self._RPM_int(process_handle, cg_t_ptr)
-        print "Found CG_T ptr 0x%x and 0x%x, should be 0x%x" % (cg_t_ptr, self.CG_T, CG_T)
-        self.REFDEF = self.CG_T + 0x42EC0
-        print "Calculated REFDEF 0x%x, should be 0x%x" % (self.REFDEF, REFDEF)
-        
-        cgs_t_ptr = self._get_int_in_raw(raw, self.addr["cgs_t"] + 6)
-        self.CGS_T = self._RPM_int(process_handle, cgs_t_ptr)
-        print "Found CGS_T ptr 0x%x and 0x%x, should be 0x%x" % (cgs_t_ptr, self.CGS_T, CGS_T)
-        
-        entities_ptr = self._get_int_in_raw(raw, self.addr["entities"] + 49)
-        self.ENTITY = self._RPM_int(process_handle, entities_ptr)
-        print "Found ENTITIES ptr 0x%x and 0x%x, should be 0x%x" % (entities_ptr, self.ENTITY, ENTITY)
-        self.CLIENTINFO = self.ENTITY - 0x8CFF4
-        print "Calculated CLIENTINFO 0x%x, should be 0x%x" % (self.CLIENTINFO, CLIENTINFO)
-        
-        return
-        addr = self.addr["cvar"]
-        self.cvar_phys_drawDebugInfo = self._get_int_in_raw(raw, addr + 1)
-        self.ptr_access_phys_drawDebugInfo = addr + 5
-        self.ptr_jmpto_phys_drawDebugInfo = addr + 20
-        
-        self.GetTagInfoForClientNum = self.addr["GetTagInfoForClientNum"]
-        self.GetTagForClientNumInfo = self.addr["GetTagForClientNumInfo"]
-        
-        self.CG_Trace = self.addr["CG_Trace"]
-        self.cg_time = self._get_int_in_raw(raw, self.addr["cg_time"] + 12) 
-        self.ps_clientNum = self._get_int_in_raw(raw, self.addr["ps_clientNum"] + 1)
-        self.refdef = self._get_int_in_raw(raw, self.addr["refdef"] + 14)
-        self.dvars = self._get_int_in_raw(raw, self.addr["dvars"] + 8)
-        self.num_dvars = self._get_int_in_raw(raw, self.addr["dvars"] + 31)
-        self.centity_t_size = self._get_int_in_raw(raw, self.addr["cg_entities"] + 10)
-        self.cg_entities = self._get_int_in_raw(raw, self.addr["cg_entities"] + 20)
-        
-        print "cvar_phys_drawDebugInfo=%x" % self.cvar_phys_drawDebugInfo
-        print "ptr_access_phys_drawDebugInfo=%x" % self.ptr_access_phys_drawDebugInfo
-        print "ptr_jmpto_phys_drawDebugInfo=%x" % self.ptr_jmpto_phys_drawDebugInfo
-        print "GetTagInfoForClientNum=%x" % self.GetTagInfoForClientNum
-        print "GetTagForClientNumInfo=%x" % self.GetTagForClientNumInfo
-        print "CG_Trace=%x" % self.CG_Trace
-        print "cg_time=%x" % self.cg_time
-        print "ps_clientNum=%x" % self.ps_clientNum
-        print "refdef=%x" % self.refdef
-        print "dvars=%x" % self.dvars
-        print "num_dvars=%x" % self.num_dvars
-        print "centity_t_size=%x" % self.centity_t_size
-        print "cg_entities=%x" % self.cg_entities
-        
+            print "===> All offsets seem ok."
+            print "----------------------------------------"
+            ok = True
+            
     def _RPM(self, process_handle, address, buffer):
         if not windll.kernel32.ReadProcessMemory(process_handle, address, byref(buffer), sizeof(buffer), None): #@UndefinedVariable
             raise ExitingException("Could not ReadProcessMemory: ", windll.kernel32.GetLastError()) #@UndefinedVariable
